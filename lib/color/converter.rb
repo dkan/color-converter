4de7a1509ceef54cf8e01ffb05f537741e8050a3
Damien <damien.kan@gmail.com>
2013-06-15 19:37:05 -0700

    Basic functionality and testing.

---
+++ lib/color/converter.rb
@@ -0,0 +1,7 @@
+require "color/converter/version"
+
+module Color
+  module Converter
+    # Your code goes here...
+  end
+end
---
+++ lib/color/converter/version.rb
@@ -0,0 +1,5 @@
+module Color
+  module Converter
+    VERSION = "0.0.1"
+  end
+end
